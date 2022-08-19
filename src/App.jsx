4306243d19deb882a29cc8d26cd81044027c9612
Stecklain <juanmstecklain@gmail.com>
2022-08-19 10:30:48 -0300

    netlify deployment changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Inicio from './paginas/Inicio';
 import NuevoCliente from './paginas/NuevoCliente';
 import EditarCliente from './paginas/EditarCliente';
 import VerCliente from './paginas/VerCliente';
+import { Navigate } from 'react-router-dom';
 
 function App() {
-
-  
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/'
+            element={
+              <Navigate to="/clientes" replace={true} />
+            }
+        />
         <Route path='/clientes' element={<Layout />}>
           <Route index element={<Inicio />}/>
           <Route path='nuevo' element={<NuevoCliente />}/>
